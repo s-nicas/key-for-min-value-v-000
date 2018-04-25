356fa1fedd3042f655749f23909e2eaca688898a
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-25 22:13:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.each do |key, value|
-    if value<min_value || min_value = nil 
+    if min_value = nil value<min_value 
       min_value = value
       min_key = key 
     end 
